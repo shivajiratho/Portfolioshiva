aa46caa416dbe07391daa336f92d8d84b4b851e3
Truey95 <upscalevisionmedia@gmail.com>
2023-08-28 20:38:52 -0400

    add photo to logo navbar

--- components/navbars/NavbarLogoGlobal.tsx
+++ components/navbars/NavbarLogoGlobal.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import NavLogo from "../../public/assets/navbarLogo.png";
+import NavLogo from "../../public/assets/monk.png";
 
 // global navbar logo
 export default function NavbarLogoGlobal(): JSX.Element {
