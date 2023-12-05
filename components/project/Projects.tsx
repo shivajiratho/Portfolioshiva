3ba014230a1cb1b79686bf22bba95a72e83ffe02
Truey95 <upscalevisionmedia@gmail.com>
2023-12-05 14:38:43 -0500

    changed project cover photo with starloworld

--- components/project/Projects.tsx
+++ components/project/Projects.tsx
@@ -54,7 +54,7 @@ function ProjectsContainer(): JSX.Element {
       />
 
       <ProjectItem
-        backgroundImg={openAiImg}
+        backgroundImg={StarlocoverImg}
         projectUrl="/open-ai"
         tech="OpenAI, NodeJS, ExpressJS, Render"
         title="Open AI Image Generator"
