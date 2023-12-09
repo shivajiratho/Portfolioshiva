03b20cb469c38f46363455385e0e84b1d25bfa65
Truey95 <upscalevisionmedia@gmail.com>
2023-12-09 13:02:02 -0500

    changed font size

--- components/heros/HeroText.tsx
+++ components/heros/HeroText.tsx
@@ -3,14 +3,14 @@ export default function HeroText(): JSX.Element {
   return (
     <>
       <div>
-        <p className="uppercase text-sm tracking-widest text-gray-600">
+        <p className="uppercase text-lg tracking-widest text-gray-600">
           Let&apos;s build web applications!
         </p>
       </div>
 
-      <h4 className="py-4 text-grey-700">
+      <h6 className="py-4 text-grey-700">
         Hello, I&apos;m <span className="text-red-600">Trevor Smith</span>
-      </h4>
+      </h6>
       <h1 className="py-4 text-gray-700">Web Developer</h1>
       <p className="py-4 text-gray-600 max-w-xs m-auto">
         Focused on building responsive, mobile first web applications while
