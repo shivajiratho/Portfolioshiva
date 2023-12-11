7ba39bb3ebf720642313fa2fb9b2b8a45a890f3c
Truey95 <upscalevisionmedia@gmail.com>
2023-12-11 17:48:34 -0500

    made new nike mock up page

--- pages/nikemockup.tsx
+++ pages/nikemockup.tsx
@@ -1,7 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
-
-import nikemockupImg from "../public/assets/projects/nikemockup.png";
 
 import BackButton from "../components/BackButton";
 import ProjectButtonCode from "../components/project/ProjectButtonCode";
@@ -11,51 +8,37 @@ import ProjectHeroText from "../components/project/ProjectHeroText";
 import ProjectTextHeading from "../components/project/ProjectTextBody";
 import TechStack from "../components/TechStack";
 
+import nikemockupImg from "../public/assets/projects/nikemockup.png";
+
 const techStack = [
-  "Hyper 3D",
-  "JavaScript",
-  
+  "GatsbyJs",
+  "GatsbyCloud",
+  "GraphQL",
+  "Contentful CMS",
+  "React-Helmet",
+  "Slugify",
 ] as const;
 
 // body of text found in project pages
-function WeatherTextBody(): JSX.Element {
+function RecipeTextBody() {
   return (
     <div className="col-span-4">
       <ProjectTextHeading />
       <p>
-        My favorite and personally most useful app, I created this app to
-        address some features I find lacking {" "}
-        <Link
-          href=""
-          target="_blank"
-          className="text-blue-500 cursor-pointer"
-        >
-          OpenWeather API.{" "}
-        </Link>
-        <span>
-          {" "}
-        </span>
-        <Link
-          href=""
-          target="_blank"
-          className="text-blue-500 cursor-pointer"
-        >
-          GeoDB Cities API.{" "}
-        </Link>
-        <span>
-          React Accordion and Pagination is used so you simply search for the
-          city, select return results, and the response will create a weather
-          forecast with collapse/expand elements for the daily forecast.
-        </span>
+        GatsbyJS was the first static site generator I used to build websites.
+        This is my favorite since it contains cooking recipes I personally use.
+        I like to cook and needed a way to keep up with recipes I enjoyed
+        making. Pages are generated with the help of Contentful CMS and GraphQL.
       </p>
-      <ProjectButtonCode url="https://github.com/milliorn/Current-Weather" />
-      <ProjectButtonDemo url="https://milliorn.github.io/Current-Weather/" />
+
+      <ProjectButtonCode url="https://github.com/milliorn/Recipe-Page" />
+      <ProjectButtonDemo url="https://gatsbyrecipes.gatsbyjs.io/" />
     </div>
   );
 }
 
-// weather page
-export default function Weather(): JSX.Element {
+// recipes page
+export default function Recipes(): JSX.Element {
   return (
     <div className="w-full">
       <div className="w-screen h-[50vh] relative">
@@ -67,13 +50,13 @@ export default function Weather(): JSX.Element {
           src={nikemockupImg}
         />
         <ProjectHeroText
-          h2="Nike Mockup"
-          h3="React, JavaScript, Tailwind CSS"
+          h2="Gatsby Recipes"
+          h3="GatsbyJs, Contentful CMS, GraphQL"
         />
       </div>
 
       <div className="max-w-7xl mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
-        <WeatherTextBody />
+        <RecipeTextBody />
         <TechStack data={techStack} />
         <BackButton />
       </div>
