f49646050b524fcc43ae2b6b9273d72cb786c85f
Truey95 <upscalevisionmedia@gmail.com>
2023-12-11 14:12:36 -0500

    fixed text body

--- pages/holsolwell.tsx
+++ pages/holsolwell.tsx
@@ -62,7 +62,7 @@ export default function Dictionary(): JSX.Element {
       </div>
 
       <div className="max-w-7xl mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
-        <holsolwellTextBody />
+        <DictionaryTextBody />
         <TechStack data={techStack} />
         <BackButton />
       </div>
