f0f19f7dac198fa1577ec8612486fb7e2bd3407a
Truey95 <upscalevisionmedia@gmail.com>
2023-12-11 14:07:25 -0500

    switched all directory

--- pages/holsolwell.tsx
+++ pages/holsolwell.tsx
@@ -31,13 +31,13 @@ function DictionaryTextBody(): JSX.Element {
         one of those occasions using Material-UI. Using this framework I built
         my first project with a Light-Dark mode toggle.{" "}
         <Link
-          href="https://dictionaryapi.dev/"
+          href=""
           target="_blank"
           className="text-blue-500 cursor-pointer"
         >
-          Free Dictionary API{" "}
+          {" "}
         </Link>
-        <span>was used here to get results in 12 langauges.</span>
+        <span></span>
       </p>
 
       <ProjectButtonCode url="https://github.com/milliorn/UI-Dictionary" />
@@ -56,13 +56,13 @@ export default function Dictionary(): JSX.Element {
           alt="/"
           className="absolute z-10 object-cover"
           fill
-          src={dictionaryImg}
+          src={holsolwellcovercoverImg}
         />
-        <ProjectHeroText h2="Dictionary" h3="React, JavaScript, Material-UI" />
+        <ProjectHeroText h2="Hol Sol Wellness" h3="React, JavaScript, Material-UI" />
       </div>
 
       <div className="max-w-7xl mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
-        <DictionaryTextBody />
+        <holsolwellTextBody />
         <TechStack data={techStack} />
         <BackButton />
       </div>
