dcaa5889f767c993a2050b963974559adbff425a
Truey95 <upscalevisionmedia@gmail.com>
2023-12-11 17:22:20 -0500

    added nikemockup

--- pages/nikemockup.tsx
+++ pages/nikemockup.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import currentWeatherImg from "../public/assets/projects/current-weather.png";
+import nikemockupImg from "../public/assets/projects/nikemockup.png";
 
 import BackButton from "../components/BackButton";
 import ProjectButtonCode from "../components/project/ProjectButtonCode";
@@ -12,12 +12,8 @@ import ProjectTextHeading from "../components/project/ProjectTextBody";
 import TechStack from "../components/TechStack";
 
 const techStack = [
-  "React",
+  "Hyper 3D",
   "JavaScript",
-  "Accordion",
-  "Paginate",
-  "Tailwind CSS",
-  "API",
 ] as const;
 
 // body of text found in project pages
@@ -30,27 +26,24 @@ function WeatherTextBody(): JSX.Element {
         address some features I find lacking in other apps. I can now check the
         weather reported from thousands of cities worldwide using{" "}
         <Link
-          href="https://openweathermap.org/"
+          href=
           target="_blank"
           className="text-blue-500 cursor-pointer"
         >
-          OpenWeather API.{" "}
+          {" "}
         </Link>
         <span>
-          API is able to get precision results by using latitude/longitude in
-          the request, which is provided by the{" "}
+         {" "}
         </span>
         <Link
-          href="https://rapidapi.com/wirefreethought/api/geodb-cities"
+          href=
           target="_blank"
           className="text-blue-500 cursor-pointer"
         >
-          GeoDB Cities API.{" "}
+          {" "}
         </Link>
         <span>
-          React Accordion and Pagination is used so you simply search for the
-          city, select return results, and the response will create a weather
-          forecast with collapse/expand elements for the daily forecast.
+          
         </span>
       </p>
       <ProjectButtonCode url="https://github.com/milliorn/Current-Weather" />
@@ -69,11 +62,11 @@ export default function Weather(): JSX.Element {
           alt="/"
           className="absolute z-10 object-cover"
           fill
-          src={currentWeatherImg}
+          src={nikemockupImg}
         />
         <ProjectHeroText
-          h2="Current Weather"
-          h3="React, JavaScript, Tailwind CSS"
+          h2="Nike Mock Up"
+          h3="Wordpress, JavaScript, Hyper 3D"
         />
       </div>
 
