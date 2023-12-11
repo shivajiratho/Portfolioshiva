31a398f9bbd4112a606df0b89b499919b5242cd2
Truey95 <upscalevisionmedia@gmail.com>
2023-12-11 14:11:22 -0500

    sedrfgh

--- pages/holsolwell.tsx
+++ pages/holsolwell.tsx
@@ -56,13 +56,13 @@ export default function Dictionary(): JSX.Element {
           alt="/"
           className="absolute z-10 object-cover"
           fill
-          src={holsolwellcovercoverImg}
+          src={holsolwellcoverImg}
         />
         <ProjectHeroText h2="Hol Sol Wellness" h3="React, JavaScript, Material-UI" />
       </div>
 
       <div className="max-w-7xl mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
-        <DictionaryTextBody />
+        <holsolwellTextBody />
         <TechStack data={techStack} />
         <BackButton />
       </div>
