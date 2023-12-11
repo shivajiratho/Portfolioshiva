f2320af0cb5c84210dc8bb3ca9d1f317058bd89f
Truey95 <upscalevisionmedia@gmail.com>
2023-12-10 19:24:34 -0500

    added paragraph

--- pages/movie-database.tsx
+++ pages/movie-database.tsx
@@ -12,13 +12,9 @@ import ProjectTextHeading from "../components/project/ProjectTextBody";
 import TechStack from "../components/TechStack";
 
 const techStack = [
-  "React",
-  "Netlify",
+  "Javascript",
+  "PHP",
   "TypeScript",
-  "Prop Types",
-  "Styled Components",
-  "Router",
-  "TMDB API",
 ] as const;
 
 // body of text found in project pages
@@ -27,10 +23,15 @@ function MovieDatabaseTextBody(): JSX.Element {
     <div className="col-span-4">
       <ProjectTextHeading />
       <p>
-        My first venture into TypeScript, React Router, Styled Components, and
-        Prop Types. I learned numerous things on this project during a time when
-        React was beginning to migrate away from FunctionComponent and Classes
-        into Components and Props as seen{" "}
+      I meticulously establish a secure database using the MySQL Database Wizard, carefully assigning user privileges for enhanced security. 
+      I proceed to upload the application files via the cPanel File Manager or an FTP client, ensuring file permissions are correctly 
+      set to safeguard against unauthorized access. 
+      To boost the application's capabilities, I employ EasyApache to install necessary PHP extensions, and I automate routine maintenance 
+      tasks like database backups by setting up cron jobs.
+       I prioritize security by installing an SSL certificate through cPanel's SSL/TLS Manager and by configuring .htaccess for additional 
+       protective measures such as IP whitelisting. By utilizing cPanel's comprehensive tools, I adeptly manage and maintain a secure, efficient, 
+       and user-friendly Passhub PHP application,
+       ensuring reliable password management for users.{" "}
         <Link
           href="https://reactjs.org/docs/components-and-props.html"
           target="_blank"
@@ -69,7 +70,7 @@ export default function MovieDatabase(): JSX.Element {
           fill
           src={passhubcoverImg}
         />
-        <ProjectHeroText h2="Pass Hub" h3="Javascript, Wordpress, PHP" />
+        <ProjectHeroText h2="Pass Hub" h3="Javascript, Wordpress, PHP, Typescript" />
       </div>
 
       <div className="max-w-7xl mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
