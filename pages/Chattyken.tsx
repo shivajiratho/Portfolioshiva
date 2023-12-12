4987ec6036752279dfc8f87763c7437296b7f7f7
Truey95 <upscalevisionmedia@gmail.com>
2023-12-11 23:00:57 -0500

    went back to blank page

--- pages/Chattyken.tsx
+++ pages/Chattyken.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import chattykenportfileImg from "../public/assets/projects/chattykenportfile.png";
+import AmistadcityImg from "../public/assets/projects/Amistadcity.png";
 
 import BackButton from "../components/BackButton";
 import ProjectButtonCode from "../components/project/ProjectButtonCode";
@@ -13,40 +13,45 @@ import TechStack from "../components/TechStack";
 
 const techStack = [
   "JavaScript",
-  "Hyper 3D"
+  "Rev Slider",
 ] as const;
 
 // body of text found in project pages
-function DictionaryTextBody(): JSX.Element {
+function LandingPageTextBody() {
   return (
     <div className="col-span-4">
       <ProjectTextHeading />
       <p>
-      Our team is meticulously constructing "Chatty Ken," a versatile web application 
-      that will host an array of intelligent bots, each engineered for specialized tasks, 
-      and all refined through the application of advanced machine learning techniques. 
-      These bots, driven by deep learning and enhanced with strategic AI prompting, 
-      are set to offer a rich, adaptive, and context-sensitive dialogue, enabling them 
-      to assist with an extensive range of activities, from scheduling and information 
-      retrieval to expert support in complex domains.{" "}
+        I used UX/UI experience to build this festival website with various technologies and frameworks. SpaceX landing page was
+        made with{" "}
         <Link
-          href=""
+          href="https://milliorn.github.io/SpaceX-Website/"
           target="_blank"
           className="text-blue-500 cursor-pointer"
         >
-          {" "}
+          AstroJs{" "}
         </Link>
-        <span></span>
+        <span>
+          Newest framework I have begun to use is NextJs which is what I used to
+          build this{" "}
+        </span>
+        <Link
+          href="https://github.com/Truey95"
+          target="_blank"
+          className="text-blue-500 cursor-pointer"
+        >
+          portfoilio.
+        </Link>
       </p>
 
-      <ProjectButtonCode url="https://github.com/milliorn/UI-Dictionary" />
-      <ProjectButtonDemo url="https://react-material-ui-dictionary.netlify.app/" />
+      <ProjectButtonCode url="https://github.com/milliorn?tab=repositories&q=landing&type=&language=&sort=" />
+      <ProjectButtonDemo url="https://www.amistadcity.com" />
     </div>
   );
 }
 
-// dictionary page
-export default function Dictionary(): JSX.Element {
+// projects landing page
+export default function LandingPage(): JSX.Element {
   return (
     <div className="w-full">
       <div className="w-screen h-[50vh] relative">
@@ -55,13 +60,13 @@ export default function Dictionary(): JSX.Element {
           alt="/"
           className="absolute z-10 object-cover"
           fill
-          src={chattykenportfileImg}
+          src={AmistadcityImg}
         />
-        <ProjectHeroText h2="Chatty Ken" h3="Openai, PHP, Javascript, Stripe, Javascript" />
+        <ProjectHeroText h2="Amistad City" h3="React, Tailwind CSS, Rev Slider, Wordpress" />
       </div>
 
       <div className="max-w-7xl mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
-        <DictionaryTextBody />
+        <LandingPageTextBody />
         <TechStack data={techStack} />
         <BackButton />
       </div>
