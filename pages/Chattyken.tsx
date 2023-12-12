3eafa1062f05b429ad0e10749638d5f4939e51f7
Truey95 <upscalevisionmedia@gmail.com>
2023-12-12 02:50:50 -0500

    remade chatty ken

--- pages/Chattyken.tsx
+++ pages/Chattyken.tsx
@@ -13,18 +13,23 @@ import TechStack from "../components/TechStack";
 
 const techStack = [
   "JavaScript",
-  "PHP",
-  "Typescript",
+  "Tailwind",
   "OpenAI",
+  "Typescript",
 ] as const;
 
 // body of text found in project pages
-function LandingPageTextBody() {
+function DictionaryTextBody(): JSX.Element {
   return (
     <div className="col-span-4">
       <ProjectTextHeading />
       <p>
-      {" "}
+      As the sole architect behind Hol Sol Wellness,
+       a health-focused e-commerce website, I meticulously crafted a 
+       seamless online experience using WordPress and the Divi Builder, 
+       integrated immersive 3D visualizations with Hyper3D, and implemented a 
+       secure Stripe payment system to ensure a smooth, engaging, and 
+       trustworthy shopping journey for wellness enthusiasts.{" "}
         <Link
           href=""
           target="_blank"
@@ -32,41 +37,31 @@ function LandingPageTextBody() {
         >
           {" "}
         </Link>
-        <span>
-          {" "}
-        </span>
-        <Link
-          href="https://github.com/Truey95"
-          target="_blank"
-          className="text-blue-500 cursor-pointer"
-        >
-        
-        </Link>
+        <span></span>
       </p>
 
-      
-      <ProjectButtonDemo url="https://www.chattyken.com" />
+      <ProjectButtonDemo url="https://chattyken.com" />
     </div>
   );
 }
 
-// projects landing page
-export default function LandingPage(): JSX.Element {
+// dictionary page
+export default function Dictionary(): JSX.Element {
   return (
     <div className="w-full">
       <div className="w-screen h-[50vh] relative">
         <ProjectHeroOverlay />
         <Image
-          alt=""
+          alt="/"
           className="absolute z-10 object-cover"
           fill
           src={chattykenportfileImg}
         />
-        <ProjectHeroText h2="Chatty Ken" h3="PHP, Javascript, OpenAI, Typescript" />
+        <ProjectHeroText h2="Chatty Ken" h3="Tailwind, PHP ,Javascript, Stripe, Typescript" />
       </div>
 
       <div className="max-w-7xl mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
-        <LandingPageTextBody />
+        <DictionaryTextBody />
         <TechStack data={techStack} />
         <BackButton />
       </div>
