4a6e59bb4d8891f43d0bb8a4d410018c0b66c2aa
Truey95 <upscalevisionmedia@gmail.com>
2023-12-12 12:41:06 -0500

    removed nav icons

--- components/navbars/NavbarBottomSideMenu.tsx
+++ components/navbars/NavbarBottomSideMenu.tsx
@@ -22,10 +22,13 @@ export default function NavbarBottomSideMenu(): JSX.Element {
       className:
         "rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300 text-sky-500",
       href: "https://twitter.com",
-      icon: <FaTwitter />,
+      icon: 
     },
     {
-
+      className:
+        "rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300 text-pink-500",
+      href: "https://milliorn.github.io/digital-resume/",
+      icon:
     },
   ] as const;
 
